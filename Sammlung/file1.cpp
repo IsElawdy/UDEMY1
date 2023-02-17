28c762b069a0334d88c0bbd1a108567230386021
IsElawdy <iislamelawdy@gmail.com>
2023-02-17 20:26:36 +0100

    i.O

--- Sammlung/file1.cpp
+++ Sammlung/file1.cpp
@@ -1,5 +1,5 @@
 #include <fstream>
-#include <libc.h> // fuer exit()
+#include <libc.h>      // fuer exit()
 using namespace std;
 
 void error(char *s)
@@ -10,20 +10,21 @@ void error(char *s)
 
 main()
 {
-  ifstream von("test"); // Anlegen eines Objektes der
-                        // Klasse ifstream
-  if (!von)
-    error("Fehler beim Oeffnen der Quelle");
-  ofstream nach("ziel"); // Anlegen eines Objektes der
-                         // Klasse ofstream
-  if (!nach)
-    error("Fehler beim Oeffnen des Ziels");
-  char c;
-  while (von.get(c)) // solange ohne Fehler
-    nach.put(c);     // zeichenweise kopieren
-  if (!von.eof())
-    error("Fehler beim Kopieren Quelle ");
-  von.close();
-  nach.close();
-  return (0);
-}
+    ifstream von("test");  // Anlegen eines Objektes der
+                             // Klasse ifstream
+    if(!von)
+      error("Fehler beim Oeffnen der Quelle");
+    ofstream nach("ziel");   // Anlegen eines Objektes der
+                             // Klasse ofstream
+    if(!nach)
+      error("Fehler beim Oeffnen des Ziels");
+    char c;
+    while(von.get(c))    // solange ohne Fehler
+      nach.put(c);       // zeichenweise kopieren
+   if(!von.eof())
+      error("Fehler beim Kopieren Quelle ");
+   von.close();
+   nach.close();
+    return(0);
+   }
+
